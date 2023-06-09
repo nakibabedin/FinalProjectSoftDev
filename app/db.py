489f47b2f11fb850f1569bad470c96fd2446d2af
nakibabedin <nabedin30@stuy.edu>
2023-06-08 20:21:02 -0400

    add database init

--- app/db.py
+++ app/db.py
@@ -11,3 +11,18 @@ def db_connect():
 def db_close():
     db.commit()
     db.close()
+
+def tweet_table_init():
+    '''
+    Tweet Components:
+    1) Profile Picture
+    2) Username
+    3) Name
+    4) Tweet content
+    4) Likes
+    '''
+    c =  db_connect()
+    c.execute("CREATE TABLE IF NOT EXISTS stories (pfp text, username text, name text, content text, likes int)")
+    db_close()
+
+
